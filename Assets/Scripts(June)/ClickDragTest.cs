b256034f7305b39c6297dc6faf4ba60ae71b339a
ClothedInStars <123790057+ClothedInStars@users.noreply.github.com>
2023-09-28 17:59:09 -0500

    testing

---
+++ Assets/Scripts(June)/ClickDragTest.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDragTest : MonoBehaviour
+{
+
+
+    //FIX ALL OF THIS TO BE ATTACHED TO THE THINGAMABOB YOU're
+
+
+
+    //Stores the object this script is tied to
+
+    //Stores the object it's colliding with at the moment
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+
+    private void OnMouseDown()
+    {
+
+        //"Grabs" what you're hovering over
+    }
+
+
+    private void OnMouseDrag()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        
+        //Pass heldObject into other collided object
+        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        //Whenever you hover over an object with collision, this stores a reference to it
+        
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+}
