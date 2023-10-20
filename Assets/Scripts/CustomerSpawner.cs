347d07f0541b6658867be79935889f1ea081869f
Ian-Bohanan <icb0003@uah.edu>
2023-10-20 00:42:47 -0500

    Revert "Merge pull request #8 from IanBohanan/FoodItem"
    This reverts commit 621541dd5f75c8010559855a44aafc8f95537b22, reversing
    changes made to 1106a98db52a6d962fabe0aa3ec6c6a105e7f62f.

--- Assets/Scripts/CustomerSpawner.cs
+++ Assets/Scripts/CustomerSpawner.cs
@@ -4,57 +4,19 @@ using UnityEngine;
 
 public class CustomerSpawner : MonoBehaviour
 {
-    //The different spawners can hold a customer.
-    private class Spawner
-    {
-        public Vector3 point; //Where the customer will spawn
-        public bool filled; //Whether there is a customer in that spot or not
-    }
-
     public GameObject customerPrefab; //The customer prefab to spawn
-
-    [SerializeField]
-    private List<Transform> spawnlocations; //Where should the customer spawn
-    private List<Spawner> spawnPoints; //The actual spawn points used to create the customer line
-
+    public Transform customerSpawnPoint; //Where should the customer spawn
     public float timeUntilSpawn = 5.0f;
-    public float timeLeft; //Time in seconds until something happens
-
-    private void OnEnable()
-    {
-        Customer.leftLine += freeLineSpot; //Subscribe to customer's sitting event so it can free a spot in line when customer leaves line
-    }
-
-    private void OnDisable()
-    {
-        Customer.leftLine -= freeLineSpot;
-    }
+    private float timeLeft; //Time in seconds until something happens
 
     // Start is called before the first frame update
     void Start()
     {
         timeLeft = timeUntilSpawn;
-        initSpawnPoints(); //Create the customer spawn points
-
-    }
-
-    //Creates the spawn locations based on how many spawnLocations were given
-    //Makes them all empty and assigns them locations based on each spawnLocation
-    private void initSpawnPoints()
-    {
-        spawnPoints = new List<Spawner>();
-        foreach (Transform loc in spawnlocations)
-        {
-            Spawner curSpawner = new Spawner();
-            curSpawner.filled = false;
-            curSpawner.point = loc.position;
-            spawnPoints.Add(curSpawner);
-        }
     }
 
     //Updates the timer every frame.
-    //Once the timer hits zero, attempt to spawn a customer in the line. 
-    //Finally reset the timer
+    //Once the timer reaches zero, the customer leaves and notifies the game that they left
     private void updateTimer()
     {
         timeLeft -= Time.deltaTime;
@@ -62,31 +24,8 @@ public class CustomerSpawner : MonoBehaviour
         if (timeLeft <= 0.0f)
         {
             timeLeft = timeUntilSpawn;
-            attemptSpawnCustomer();
+            GameObject nextCustomer = Instantiate(customerPrefab, customerSpawnPoint.transform.position, Quaternion.identity); //Create the new customer object
         }
-    }
-    
-    //Attempts to spawn a customer in the first available spawner in spawnPoints
-    private void attemptSpawnCustomer()
-    {
-        for(int i=0;i<spawnPoints.Count;i++)
-        {
-            if (!spawnPoints[i].filled)
-            {
-                GameObject nextCustomer = Instantiate(customerPrefab, spawnPoints[i].point, Quaternion.identity); //Create the new customer object
-                nextCustomer.transform.GetComponent<Customer>().spotInLine = i; //Tell it which spot it was in line
-                spawnPoints[i].filled = true;
-                return;
-            }
-        }
-    }
-
-    //Frees a spot in the spawnPoints list.
-    //Params: int spot - the spot in line that should be freed.
-    private void freeLineSpot(int spot)
-    {
-        print("CustomerSpawner: Freeing line spot" + spot);
-        spawnPoints[spot].filled = false;
     }
 
     // Update is called once per frame
