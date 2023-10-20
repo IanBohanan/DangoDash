74ca147b72a08c2ac2f4c78b1b8fe10b17ca7b84
ClothedInStars <123790057+ClothedInStars@users.noreply.github.com>
2023-10-20 06:43:07 -0500

    made Main menu :)

---
+++ Assets/Scripts(June)/MainMenuNewGameScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNewGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        newGame.onClick.AddListener(schmooveScenes);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [SerializeField] Button newGame;
+
+    void schmooveScenes()
+    {
+        //open the main game scene
+        SceneManager.LoadScene("SampleScene");
+    }    
+}
